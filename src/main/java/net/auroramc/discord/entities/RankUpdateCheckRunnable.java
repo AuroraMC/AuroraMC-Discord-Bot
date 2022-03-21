7a1d213599c3afcc90a3e0f8d0e63ff6c40406cc
Block2Block <accounts@block2block.me>
2022-03-21 20:06:58 +0000

    Check to see if user is member of a guild before giving a rank

--- src/main/java/net/auroramc/discord/entities/RankUpdateCheckRunnable.java
+++ src/main/java/net/auroramc/discord/entities/RankUpdateCheckRunnable.java
@@ -24,38 +24,44 @@ public class RankUpdateCheckRunnable implements Runnable {
             if (user != null) {
                 if (update.getNewRank() != null) {
                     for (Guild guild : user.getMutualGuilds()) {
-                        guild.addRoleToMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getRankMappings(guild.getIdLong()).get(update.getNewRank())))).queue();
-                        guild.removeRoleFromMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getRankMappings(guild.getIdLong()).get(update.getOldRank())))).queue();
-                        Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
-                                .setTitle("User Rank Update")
-                                .setThumbnail(user.getAvatarUrl())
-                                .addField("Old Rank", update.getOldRank().getName(), false)
-                                .addField("New Rank", update.getNewRank().getName(), false)
-                                .setTimestamp(Instant.now())
-                                .setColor(new Color(0, 170,170))
-                                .build()).queue();
+                        if (guild.isMember(user)) {
+                            guild.addRoleToMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getRankMappings(guild.getIdLong()).get(update.getNewRank())))).queue();
+                            guild.removeRoleFromMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getRankMappings(guild.getIdLong()).get(update.getOldRank())))).queue();
+                            Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
+                                    .setTitle("User Rank Update")
+                                    .setThumbnail(user.getAvatarUrl())
+                                    .addField("Old Rank", update.getOldRank().getName(), false)
+                                    .addField("New Rank", update.getNewRank().getName(), false)
+                                    .setTimestamp(Instant.now())
+                                    .setColor(new Color(0, 170,170))
+                                    .build()).queue();
+                        }
                     }
                 } else if (update.getAddedSubrank() != null) {
                     for (Guild guild : user.getMutualGuilds()) {
-                        guild.addRoleToMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getSubrankMappings(guild.getIdLong()).get(update.getAddedSubrank())))).queue();
-                        Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
-                                .setTitle("User SubRank Update")
-                                .setThumbnail(user.getAvatarUrl())
-                                .addField("SubRank Added", update.getAddedSubrank().getName(), false)
-                                .setTimestamp(Instant.now())
-                                .setColor(new Color(85, 255,85))
-                                .build()).queue();
+                        if (guild.isMember(user)) {
+                            guild.addRoleToMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getSubrankMappings(guild.getIdLong()).get(update.getAddedSubrank())))).queue();
+                            Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
+                                    .setTitle("User SubRank Update")
+                                    .setThumbnail(user.getAvatarUrl())
+                                    .addField("SubRank Added", update.getAddedSubrank().getName(), false)
+                                    .setTimestamp(Instant.now())
+                                    .setColor(new Color(85, 255,85))
+                                    .build()).queue();
+                        }
                     }
                 } else if (update.getRemovedSubrank() != null) {
                     for (Guild guild : user.getMutualGuilds()) {
-                        guild.removeRoleFromMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getSubrankMappings(guild.getIdLong()).get(update.getAddedSubrank())))).queue();
-                        Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
-                                .setTitle("User SubRank Update")
-                                .setThumbnail(user.getAvatarUrl())
-                                .addField("SubRank Removed", update.getRemovedSubrank().getName(), false)
-                                .setTimestamp(Instant.now())
-                                .setColor(new Color(255, 85,85))
-                                .build()).queue();
+                        if (guild.isMember(user)) {
+                            guild.removeRoleFromMember(user.getIdLong(), Objects.requireNonNull(guild.getRoleById(GuildManager.getSubrankMappings(guild.getIdLong()).get(update.getAddedSubrank())))).queue();
+                            Objects.requireNonNull(guild.getTextChannelById(GuildManager.getLinkLogId(guild.getIdLong()))).sendMessageEmbeds(new EmbedBuilder()
+                                    .setTitle("User SubRank Update")
+                                    .setThumbnail(user.getAvatarUrl())
+                                    .addField("SubRank Removed", update.getRemovedSubrank().getName(), false)
+                                    .setTimestamp(Instant.now())
+                                    .setColor(new Color(255, 85,85))
+                                    .build()).queue();
+                        }
                     }
                 }
             }
