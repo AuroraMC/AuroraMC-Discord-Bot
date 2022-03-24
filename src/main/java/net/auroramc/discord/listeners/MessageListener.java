9c8c81ce5d917cdd6af23c3c58adefb49b8dadfa
Block2Block <accounts@block2block.me>
2022-03-24 14:03:31 +0000

    Add some logging messages

--- src/main/java/net/auroramc/discord/listeners/MessageListener.java
+++ src/main/java/net/auroramc/discord/listeners/MessageListener.java
@@ -8,6 +8,7 @@ import net.auroramc.discord.DiscordBot;
 import net.auroramc.discord.commands.CommandLink;
 import net.auroramc.discord.managers.CommandManager;
 import net.auroramc.discord.managers.GuildManager;
+import net.auroramc.discord.managers.MessageCache;
 import net.auroramc.discord.managers.SpamManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
@@ -40,7 +41,9 @@ public class MessageListener extends ListenerAdapter {
                 CommandManager.onCommand(e.getMessage(), e.getMember());
             } else {
                 //Spam manager
-                SpamManager.onMessage(e.getMessage());
+                if (!SpamManager.onMessage(e.getMessage())) {
+                    MessageCache.onMessage(e.getMessage());
+                }
             }
         } else if (e.getMessage().getContentStripped().startsWith("!link")) {
             List<String> args = new ArrayList<>(Arrays.asList(e.getMessage().getContentStripped().split(" ")));
@@ -57,22 +60,35 @@ public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageDelete(@NotNull MessageDeleteEvent event) {
         if (event.isFromGuild() && event.isFromType(ChannelType.TEXT)) {
-            Message message = event.getTextChannel().getHistory().getMessageById(event.getMessageId());
+            Message message = MessageCache.getMessage(event.getMessageIdLong());
             if (message != null) {
                 Objects.requireNonNull(event.getGuild().getTextChannelById(GuildManager.getServerLogId(event.getGuild().getIdLong()))).sendMessageEmbeds(
                     new EmbedBuilder()
-
+                            .setTitle("Message Deleted")
+                            .setDescription("Message sent <t:" + message.getTimeCreated().toEpochSecond() + ":R> by " + message.getAuthor().getAsMention() + " was deleted in " + message.getChannel().getAsMention() + "\n" +
+                                    "**Message: `" + message.getContentStripped() + "`**")
+                            .setColor(new Color(0, 170, 170))
                             .build()
                 ).queue();
             }
-
         }
     }
 
     @Override
     public void onMessageUpdate(@NotNull MessageUpdateEvent event) {
-        if (event.isFromGuild()) {
-
+        if (event.isFromGuild() && event.isFromType(ChannelType.TEXT)) {
+            Message message = MessageCache.getMessage(event.getMessageIdLong());
+            if (message != null) {
+                Objects.requireNonNull(event.getGuild().getTextChannelById(GuildManager.getServerLogId(event.getGuild().getIdLong()))).sendMessageEmbeds(
+                        new EmbedBuilder()
+                                .setTitle("Message Edited")
+                                .setDescription("Message sent <t:" + message.getTimeCreated().toEpochSecond() + ":R> by " + message.getAuthor().getAsMention() + " was edited in " + message.getChannel().getAsMention() + "\n" +
+                                        "**Old Message: `" + message.getContentStripped() + "`**\n" +
+                                        "**New Message: `" + event.getMessage().getContentStripped() + "`")
+                                .setColor(new Color(0, 170, 170))
+                                .build()
+                ).queue();
+            }
         }
     }
 }
